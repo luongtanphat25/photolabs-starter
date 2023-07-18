c78f0d0a4ba790ee0bd8469c770adc88e711449a
Phat Luong <44761311+luongtanphat25@users.noreply.github.com>
2023-07-17 23:18:13 -0400

    fixed similar photos

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -9,15 +9,35 @@ import '../styles/PhotoDetailsModal.scss';
 
 const PhotoDetailsModal = (props) => {
   const { photo, favPhotos, onFavPhoto, onPhotoClick } = props;
-  const similar_photos = photo.similarPhotos;
-  const isFav = Boolean(favPhotos[photo.id]); 
+  const similar_photos = photo.similar_photos;
+  const isFav = Boolean(favPhotos[photo.id]);
+  console.log(photo);
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button" onClick={props.onCloseModal}>
-        <svg width="24" height="24" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <button
+        className="photo-details-modal__close-button"
+        onClick={props.onCloseModal}
+      >
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 18 18"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <g clipPath="url(#clip0_428_287)">
-            <path d="M14.0625 3.9375L3.9375 14.0625" stroke="black" strokeLinecap="round" strokeLinejoin="round" />
-            <path d="M14.0625 14.0625L3.9375 3.9375" stroke="black" strokeLinecap="round" strokeLinejoin="round" />
+            <path
+              d="M14.0625 3.9375L3.9375 14.0625"
+              stroke="black"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M14.0625 14.0625L3.9375 3.9375"
+              stroke="black"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
           </g>
           <defs>
             <clipPath id="clip0_428_287">
@@ -29,10 +49,18 @@ const PhotoDetailsModal = (props) => {
 
       <div className="photo-details-modal__images">
         <PhotoFavButton isFav={isFav} onClick={() => onFavPhoto(photo)} />
-        <img className="photo-details-modal__image" src={photo.urls.full} alt="Photo" />
+        <img
+          className="photo-details-modal__image"
+          src={photo.urls.full}
+          alt="Photo"
+        />
 
         <div className="photo-list__user-details">
-          <img className="photo-list__user-profile" src={photo.user.profile} alt="Profile" />
+          <img
+            className="photo-list__user-profile"
+            src={photo.user.profile}
+            alt="Profile"
+          />
 
           <div className="photo-list__user-info">
             {photo.user.name}
@@ -44,7 +72,12 @@ const PhotoDetailsModal = (props) => {
 
         <div className="photo-details-modal__header">Related Photos</div>
 
-        <PhotoList photos={similar_photos} favPhotos={favPhotos} onFavPhoto={onFavPhoto} onPhotoClick={onPhotoClick} />
+        <PhotoList
+          photos={similar_photos}
+          favPhotos={favPhotos}
+          onFavPhoto={onFavPhoto}
+          onPhotoClick={onPhotoClick}
+        />
       </div>
     </div>
   );
